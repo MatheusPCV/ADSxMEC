3513d01105b098ede153f444f30e21a3d926b75b
Guilherme Martins <guilherme.mleal.11@gmail.com>
2023-11-23 20:37:48 -0300

    faet: swagger implementation

--- setup/urls.py
+++ setup/urls.py
@@ -15,12 +15,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+from django.conf import settings
 from Fan.urls import FanUrl
 from Fan.urls import TemperatureUrl
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API OCR",
+        default_version='v0.1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fan/', include(FanUrl)),
-    path('temperature/', include(TemperatureUrl))
+    path('temperature/', include(TemperatureUrl)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
